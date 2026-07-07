13943a2ad3c14b9e91b74f5eca8f7b49534d6c76
vpenades <5433822+vpenades@users.noreply.github.com>
2026-07-07 09:49:33 +0200

    Initial Commit

---
+++ src/InteropTypes.TensorBitmaps.Core/ITensorBitmap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics.Tensors;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteropTypes.TensorBitmaps
+{
+    /// <summary>
+    /// A bitmap interface to a bitmap backed by a <see cref="IReadOnlyTensor"/>
+    /// </summary>    
+    public interface IReadOnlyTensorBitmap
+    {
+        IReadOnlyTensor Tensor { get; }
+        TensorPixelFormat Format { get; }
+        int BytesPerPixel { get; }
+        int Width { get; }
+        int Height { get; }
+
+        ReadOnlySpan<byte> GetRowSpan(int y);
+    }
+
+    /// <summary>
+    /// A bitmap interface to a bitmap backed by a <see cref="ITensor"/>
+    /// </summary>    
+    public interface ITensorBitmap : IReadOnlyTensorBitmap
+    {
+        new ITensor Tensor { get; }
+        new Span<byte> GetRowSpan(int y);
+    }
+
+    
+}
---
+++ tests/InteropTypes.TensorBitmaps.Tests/CreationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace InteropTypes.TensorBitmaps
+{
+    internal class CreationTests
+    {
+
+        [Test]
+        public async Task TestCreateBitmaps()
+        {
+            var bmp = TensorBitmap<float, Vector3>.Create(256, 267, TensorPixelFormat.Rgb96f);
+            await Assert.That(bmp).IsNotNull();
+
+            var slice = bmp.GetCropped(new System.Drawing.Rectangle(5, 5, 16, 16));
+            await Assert.That(slice.Width).IsEqualTo(16);
+            await Assert.That(slice.Height).IsEqualTo(16);
+
+            for(int i=0; i < slice.Height; i++)
+            {
+                var row = slice.GetRowPixelsSpan(i);
+
+                await Assert.That(row.Length).IsEqualTo(16);
+            }
+
+        }
+
+
+    }
+}
